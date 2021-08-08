ab08bc1f40c53f64db3d21c59b1b469b45dab832
Roshni-Sahoo <77953223+Roshni-Sahoo@users.noreply.github.com>
2021-08-08 08:49:51 +0530

    Add files via upload

---
+++ rock_paper_scissor.js
@@ -0,0 +1,20 @@
+const game=()=>{
+	let userscore=0;
+	let compscore=0;
+	let moves=0;
+
+	const playgame=()=>{
+		const rockbutton=document.querySelector('#rock');
+		const paperbutton=document.querySelector('#paper');
+		const scissorsbutton=document.querySelector('#scissor');
+		const useropt=[rockbutton,paperbutton,scissorsbutton];
+		const compopt=['rock','paper','scissors']
+
+		useropt.forEach(option=>{
+			option.addEventListener('click',function(){
+
+				const movesLeft=document.querySelector('')
+			})
+		})
+	}
+}
